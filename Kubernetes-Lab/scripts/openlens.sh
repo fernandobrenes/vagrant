54640e6600ad57c73d58ab67d91bd21a3a46b972
Fernando Brenes <fernandobrenes@gmail.com>
2023-10-05 08:36:07 -0600

    modified openlens.sh to use .deb instead of source

--- Kubernetes-Lab/scripts/openlens.sh
+++ Kubernetes-Lab/scripts/openlens.sh
@@ -19,59 +19,11 @@ Color_Off='\033[0m'       # Text Reset
 echo -e "${Cyan}***********************************************\n
 Begin OpenLens\n
 ***********************************************${Color_Off}"
-FILE=/opt/openlens/linux-unpacked/open-lens
-if [ -f "$FILE" ]; then
-    echo "Open Lens is already installed."
-else 
-    sudo apt-get install -y make g++
-    sudo apt install libfuse2 -y
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.4/install.sh | bash
-    export NVM_DIR="$HOME/.nvm"
-    source ~/.nvm/nvm.sh
-    echo "source ~/.nvm/nvm.sh" >> ~/.bashrc
-    OPENLENS_VERSION=$(curl -s https://api.github.com/repos/lensapp/lens/releases/latest | sed -En 's/  "tag_name": "(.+)",/\1/p')
-    curl -L https://github.com/lensapp/lens/archive/refs/tags/$OPENLENS_VERSION.tar.gz | tar xvz
-    mv lens-* lens 
-    cd lens
-    nvm install 16 && nvm use 16 #&& npm install -g yarn
-    npm install -g npm@^9.6.7
-    #npm i lerna
-    npm install
-    npm run build
-    npm run build:app -- --scope=open-lens -- --dir
-    #npm run build:app
-
-    sudo mkdir /opt/openlens/
-    sudo mv open-lens/dist/linux-unpacked /opt/openlens/
-    sudo cp packages/core/build/icon.png /opt/openlens/
-    sudo chown -R vagrant:vagrant /opt/openlens/
-    #chmod +x /opt/openlens/openlens.AppImage
-    #cp packages/core/build/icon.png /opt/openlens/
-    sudo rm -rf /home/vagrant/lens
-
-    # CREATE A LAUNCHER
-    sudo mkdir /home/vagrant/.local/share/applications
-    sudo chown -R vagrant:vagrant /home/vagrant/.local/share/applications
-cat << EOF > /home/vagrant/.local/share/applications/OpenLens.desktop
-[Desktop Entry]
-Comment[en_GB]=
-Comment=Kubernetes Client
-Exec=/opt/openlens/linux-unpacked/open-lens
-GenericName[en_GB]=
-GenericName=
-Icon=/opt/openlens/icon.png
-Name[en_GB]=OpenLens
-Name=OpenLens
-NoDisplay=false
-Path=/opt/openlens/
-StartupNotify=true
-Terminal=false
-TerminalOptions=
-Type=Application
-X-DBUS-ServiceName=
-X-DBUS-StartupType=
-X-KDE-SubstituteUID=false
-X-KDE-Username=
-MimeType=x-scheme-handler/lens;text/html;
-EOF
+if command -v open-lens > /dev/null; then
+    echo -e "${Green}Open Lens is already installed${Color_Off}"
+else
+	OPENLENS_VERSION=$(curl -s https://api.github.com/repos/MuhammedKalkan/OpenLens/releases/latest | sed -En 's/  "tag_name": "(.+)",/\1/p')
+    OPENLENS_DOWNLOAD=${OPENLENS_VERSION#v}
+    wget https://github.com/MuhammedKalkan/OpenLens/releases/download/$OPENLENS_VERSION/OpenLens-$OPENLENS_DOWNLOAD.amd64.deb
+    sudo apt install ./OpenLens-$OPENLENS_DOWNLOAD.amd64.deb
 fi
